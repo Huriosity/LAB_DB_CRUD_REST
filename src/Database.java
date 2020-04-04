02fc7e1c216122610bbbe51840c39c6a67aca09c
Huriosity <sasha_gur1999@mail.ru>
2020-04-04 23:39:59 +0300

    feat: complite xmlParser.java. Now filling table fron json arrays

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,65 @@
+import java.sql.*;
+
 public class Database {
+    private static String url = System.getenv().get("db_url");
+    private static String username = System.getenv().get("db_username");
+    private static String password = System.getenv().get("db_password");
+
+    public static DatabaseResponse getAllInfoFromDatabase(){
+        ResultSet resultSet = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Connection succesfull!");
+            try (Connection connection = DriverManager.getConnection(url, username,password)){
+                System.out.println("Connection to rus_ruller DB succesfull!");
+                Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+                resultSet = statement.executeQuery("select ruller.*,ruller_town_relation.*,town.*," +
+                        "ruller_years_of_life.year_of_birth,ruller_years_of_life.year_of_death from ruller," +
+                        "ruller_town_relation,town,ruller_years_of_life WHERE (ruller.ruller_ID = " +
+                        "ruller_town_relation.foreight_ruller_ID AND ruller_town_relation.foreight_town_ID = " +
+                        "town.town_ID AND ruller_years_of_life.foreight_ruller_ID = ruller.ruller_ID);");
+                ResultSetMetaData rsmd = resultSet.getMetaData();
+                int colNum = rsmd.getColumnCount();
+                int rowNum = 0;
+                try {
+                    resultSet.last();
+                    rowNum = resultSet.getRow();
+                    resultSet.beforeFirst();
+                } catch(Exception ex) {
+                    System.err.println(ex);
+                }
+                String[] arrColNames = new String[colNum];
+                String[][] fullDataFromSet = new String[colNum][rowNum];
+                for(int i = 0; i < colNum; i++){
+                    arrColNames[i] = rsmd.getColumnName(i+1);
+                }
+                int iter = 1;
+
+                while (resultSet.next()) {// тУТ ДВИЖЕНИЕ ПО ROW
+                    for(int i = 0; i < colNum; i++){
+                        if(rsmd.getColumnClassName(i+1) == "java.lang.Integer"){
+                            try {
+                                fullDataFromSet[i][iter-1] = Integer.toString(resultSet.getInt(i+1));
+                            }catch (Exception err){
+                                System.err.println(err);
+                            }
+                        }else if(rsmd.getColumnClassName(i+1) == "java.lang.String"){
+                            try {
+                                fullDataFromSet[i][iter-1] = (String) resultSet.getString(i+1);
+                            } catch (Exception err){
+                                System.err.println(err);
+                            }
+                        }
+                    }
+                    iter++;
+                }
+                return new  DatabaseResponse(arrColNames,fullDataFromSet);
+            }
+        } catch (Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+        return null;
+    }
 }
--- src/xmlParser.java
+++ src/xmlParser.java
@@ -1,2 +1,138 @@
+import org.json.simple.JSONArray;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.sql.SQLException;
+
 public class xmlParser {
+    //DOM
+    public static void writeXML(JSONArray jsonArrayData, String filePath) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setValidating(false);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document;
+            File file = new File(filePath);
+            if (file.length() == 0) {
+                document = factory.newDocumentBuilder().newDocument();
+                Element root = document.createElement("root");
+                document.appendChild(root);
+            } else {
+                document = builder.parse(filePath);
+            }
+            document.getDocumentElement().normalize();
+
+            fillTable(document, jsonArrayData);
+            document.getDocumentElement().normalize();
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(filePath));
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(source, result);
+        } catch (SAXParseException exception) {
+            System.out.println("ERRROR");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private static void fillTable(Document doc, JSONArray jsonArrayData) throws SQLException {
+        NodeList table = doc.getElementsByTagName("table");
+        Element lang = null;
+        int i = table.getLength();
+        lang = (Element) table.item(i - 1);
+
+        Element tr_head = doc.createElement("tr");
+        String tabSymbols = "\t\t\t\t\t";
+
+        System.out.println("size = " + jsonArrayData.size());
+
+        String firstJSONObj = jsonArrayData.get(0).toString();
+
+        firstJSONObj = firstJSONObj.replaceAll("\"", "");
+        firstJSONObj = firstJSONObj.substring(1,firstJSONObj.length());
+        firstJSONObj = firstJSONObj.substring(0,firstJSONObj.length()-1);
+
+        String arr_col_names[] = firstJSONObj.split(",");
+
+        for(int it = 0;it < arr_col_names.length;it++){
+            Element th = doc.createElement("th");
+
+            th.appendChild(doc.createTextNode(arr_col_names[it]));
+
+            tr_head.appendChild(th);
+            tr_head.appendChild(doc.createTextNode("\n"));
+            tr_head.appendChild(doc.createTextNode(tabSymbols));
+
+            Element updateForm = doc.createElement("form");
+            updateForm.setAttribute("method","post");;
+            updateForm.setAttribute("id","updForm" + it);
+
+            lang.getParentNode().appendChild(updateForm);
+
+        }
+        lang.appendChild(tr_head);
+
+        for (int row = 1; row < jsonArrayData.size(); row++){
+            Element tr = doc.createElement("tr");
+            firstJSONObj = jsonArrayData.get(row).toString();
+
+            firstJSONObj = firstJSONObj.replaceAll("\"", "");
+            firstJSONObj = firstJSONObj.substring(1,firstJSONObj.length());
+            firstJSONObj = firstJSONObj.substring(0,firstJSONObj.length()-1);
+
+            String[] dataFromTable = firstJSONObj.split(",");
+
+            for(int col = 0;col < dataFromTable.length; col++){
+                Element th = doc.createElement("th");
+
+                Element inputElement = doc.createElement("input");
+                inputElement.setAttribute("name",arr_col_names[col]);
+                inputElement.setAttribute("value",dataFromTable[col]);
+                inputElement.setAttribute("size","8");
+                inputElement.setAttribute("form","updForm" + row);
+
+                if(arr_col_names[col].contains("_ID")){
+                    inputElement.setAttribute("readonly","readonly");
+                }
+
+                th.appendChild(inputElement);
+
+                tr.appendChild(th);
+                tr.appendChild(doc.createTextNode("\n"));
+                tr.appendChild(doc.createTextNode(tabSymbols));
+            }
+
+            Element th_update = doc.createElement("th");
+            Element inputUpdate = doc.createElement("input");
+            Element inputDelete = doc.createElement("input");
+
+            inputUpdate.setAttribute("type","submit");
+            inputUpdate.setAttribute("form","updForm" + row);
+            inputUpdate.setAttribute("name", "_isMethod");
+            inputUpdate.setAttribute("value","put");
+
+            inputDelete.setAttribute("type","submit");
+            inputDelete.setAttribute("form","updForm" + row);
+            inputDelete.setAttribute("name", "_isMethod");
+            inputDelete.setAttribute("value","delete");
+
+            th_update.appendChild(inputUpdate);
+            th_update.appendChild(inputDelete);
+
+            tr.appendChild(th_update);
+            lang.appendChild(tr);
+        }
+    }
 }
