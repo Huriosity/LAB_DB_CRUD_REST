b30de5ca825c52d018e3c5a181e7a27ba64140e2
Huriosity <sasha_gur1999@mail.ru>
2020-04-04 19:42:55 +0300

    feat: complite HtmlServer.java, start HtmlServerHandler.java

--- src/HtmlServer.java
+++ src/HtmlServer.java
@@ -1,2 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class HtmlServer {
+
+    private int port;
+    private String directory;
+
+    public HtmlServer(int port, String directory) {
+        this.port = port;
+        this.directory = directory;
+    }
+
+    void start() {
+        try (var server = new ServerSocket(this.port)) {
+            while (true) {
+                var socket = server.accept();
+                var thread = new HtmlServerHandler(socket, this.directory);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConfigLoader.loadConfigENV("src\\resources\\config.properties");
+        new HtmlServer(Integer.parseInt(System.getenv().get("port")), System.getenv().get("directory")).start();
+
+    }
 }
+
+
+
--- src/HtmlServerHandler.java
+++ src/HtmlServerHandler.java
@@ -1,2 +1,13 @@
-public class HtmlServerHandler {
+import java.net.Socket;
+
+public class HtmlServerHandler extends Thread {
+
+    private Socket socket;
+
+    private String directory;
+
+    HtmlServerHandler(Socket socket, String directory) {
+        this.socket = socket;
+        this.directory = directory;
+    }
 }
